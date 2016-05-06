57145e608815f157cdea901e1969ca09c93667e0
ArteneR <georgevici.ioan@yahoo.com>
2016-05-06 22:56:21 +0300

    Receive avatar image from server

--- src/awesomechatapp/Client.java
+++ src/awesomechatapp/Client.java
@@ -1,7 +1,10 @@
 package awesomechatapp;
 
+import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
@@ -19,13 +22,14 @@ public class Client {
 //	private static final String SERVER = "192.168.0.105";
 	private static final String SERVER = "raspberrypi.local";
 //	private static final String SERVER = "169.254.113.204";
-	private static final int PORT = 5003;
+	private static final int PORT = 5005;
 	private static final String CLOSE_CONNECTION = "CLOSE_CONNECTION";
 	private static final String MESSAGE = "MESSAGE";
 	private static final String RECEIVER = "ReceiverUsernameORConferenceName";
 	private Socket clientSocket;
 	private static PrintWriter outToServer;
 	private static BufferedReader inFromServer;
+        private static InputStream is;
         private BufferedReader inFromUser;
 	private String messageToSend;
 	private String messageReceived;
@@ -45,6 +49,7 @@ public class Client {
                         inFromUser = new BufferedReader(new InputStreamReader(System.in));
                         inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
                         outToServer = new PrintWriter(clientSocket.getOutputStream(),true);
+                        is = clientSocket.getInputStream();
                         
                         friends = new ArrayList<Friend>();
                 } 
@@ -87,6 +92,46 @@ public class Client {
                 String response = inFromServer.readLine();
                 System.out.println("Response from server: " + response);
                 return response;
+        }
+        
+        public static byte[] waitForFileBytes() throws IOException, InterruptedException {
+                byte[] allReceivedBytes = new byte[0];
+                byte[] readBytes = new byte[1024];
+                byte[] enlargedAllReceivedBytes;
+                
+                FileOutputStream fos = new FileOutputStream("avatar-" + username + ".png");
+                BufferedOutputStream bos = new BufferedOutputStream(fos);
+                
+                int bytesRead;
+                int totalBytes = 0;
+                while ((bytesRead = is.read(readBytes, 0, readBytes.length)) > 0) {
+                        String message = new String(readBytes, "UTF-8");
+                        if (message.contains(":")) {
+                                String responseType = message.substring(0, message.indexOf(":"));
+                                if (responseType.equals(MessageType.RESPONSE_SUCCESS.toString())) {
+                                        break;
+                                }
+                        }
+                        
+                        enlargedAllReceivedBytes = new byte[allReceivedBytes.length + 1024];
+                        for (int i = 0; i < allReceivedBytes.length; i++) {
+                                enlargedAllReceivedBytes[i] = allReceivedBytes[i];
+                        }
+                        for (int i = 0, j = allReceivedBytes.length; j < enlargedAllReceivedBytes.length; i++, j++) {
+                                enlargedAllReceivedBytes[j] = readBytes[i];
+                        }
+                        allReceivedBytes = new byte[allReceivedBytes.length + 1024];
+                        System.arraycopy(enlargedAllReceivedBytes, 0, allReceivedBytes, 0, enlargedAllReceivedBytes.length);
+                        
+                        totalBytes += bytesRead;
+                        readBytes = new byte[1024];
+                        outToServer.println(MessageType.ACK + ":" + Status.OK);
+                }
+                bos.write(allReceivedBytes, 0, totalBytes);
+                bos.flush();
+                fos.close();
+                
+                return allReceivedBytes;
         }
         
         
